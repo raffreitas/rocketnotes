d7eccb64ea2845dd0df42b95d088a3a83de04b63
Rafael Freitas <rafaarantes@live.com>
2022-07-10 12:26:19 -0300

    created new pages

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { RiShutDownLine,  } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/raffreitas.png"
+          alt="Imagem do usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Rafael Freitas</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+
+    </Container>
+  );
+}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,44 @@
+import { Container, Links, Content } from "./styles"
+
+import { ButtonText } from "../../components/ButtonText"
+import { Button } from "../../components/Button/Details"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tags"
+
+export function Details() {
+
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis illo natus recusandae quos beatae aliquam distinctio nisi at reiciendis obcaecati consequatur modi alias dicta odio, expedita quaerat optio, est labore?
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://rocketseat.com.br</a></li>
+              <li><a href="#">https://react.com</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+
+          <Button title="Voltar" loading />
+        </Content>
+      </main>
+
+    </Container>
+  )
+}
