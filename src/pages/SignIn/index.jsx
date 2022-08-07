8f4c1b2a943ca1ff8970e532e15abf471fbb3771
Rafael Freitas <rafaarantes@live.com>
2022-08-07 12:01:38 -0300

    setup axios and authentication

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,25 @@
-import { FiLogIn, FiMail, FiLock} from 'react-icons/fi'
+import { useState } from 'react';
+import { FiMail, FiLock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+
+import { useAuth } from "../../hooks/auth";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 import { Container, Form, Background } from "./styles";
 
-export function SingIn() {
-  return(
+export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
+  return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
@@ -19,15 +31,17 @@ export function SingIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">
           Criar Conta
